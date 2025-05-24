e0a4db25de3676c503f1bebdb2187c90e65e1613
Pankaj Maurya <100466858+Pankaj352@users.noreply.github.com>
2025-05-25 00:28:17 +0530

    patterns

---
+++ Star Patterns/LowerRightTriangle.js
@@ -0,0 +1,10 @@
+// *****
+//  ****
+//   ***
+//    **
+//     *
+
+let n = 5;
+for (let i = n; i > 0; i--){
+ console.log(" ".repeat(n-i)+"*".repeat(i));
+}
---
+++ Star Patterns/UpperRightTriangle.js
@@ -0,0 +1,12 @@
+ //    *
+ //   **
+ //  ***
+ // ****
+
+
+let n = 5;
+for (let i = 0; i < n; i++){
+ let space = " ";
+ console.log(space.repeat(n-i)+"*".repeat(i));
+ 
+}
